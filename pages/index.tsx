1711a6484cb8252ebab9bbfff287b325f928a82a
Rick Jones <rick.jones@playtimestudios.com>
2021-05-29 14:09:01 +0100

    WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,193 +1,177 @@
 import {
-  Card,
-  CardContent,
+  Box,
   Container,
-  CssBaseline,
+  Fade,
   Grid,
-  Link,
-  Typography,
-  makeStyles
+  Typography
 } from '@material-ui/core'
 
 import Head from 'next/head'
 import React, { ReactElement } from 'react'
-
-const gridItemClasses = makeStyles((theme) => ({
-  card: {
-    height: "100%",
-    "&:hover": {
-      color: theme.palette.primary.main,
-      borderColor: theme.palette.primary.main
-    }
-  }
-}))
-
-const GridItem = (
-  {
-    description,
-    href,
-    title
-  }: {
-    description: string,
-    href: string,
-    title: string
-  }): ReactElement => {
-  const classes = gridItemClasses();
-
-  return (
-    <Grid
-      item
-      sm={6}
-      xs={12}
-    >
-      <Link
-        href={href}
-        style={{textDecoration: "none"}}
-      >
-        <Card
-          className={classes.card}
-          variant="outlined"
-        >
-          <CardContent>
-            <Typography
-              component="h2"
-              gutterBottom
-              variant="h4"
-            >
-              {title} &rarr;
-            </Typography>
-            <Typography variant="body1">
-              {description}
-            </Typography>
-          </CardContent>
-        </Card>
-      </Link>
-    </Grid>
-  )
-}
+import { LoremIpsum } from 'react-lorem-ipsum'
+import VizSensor from 'react-visibility-sensor'
 
 export default function MuiIndex(): ReactElement {
+  const [intro1Viz, setIntro1Viz] = React.useState(false)
+  const [intro2Viz, setIntro2Viz] = React.useState(false)
+  const [intro3Viz, setIntro3Viz] = React.useState(false)
+  const [intro4Viz, setIntro4Viz] = React.useState(false)
+  const [intro5Viz, setIntro5Viz] = React.useState(false)
+  const [intro6Viz, setIntro6Viz] = React.useState(false)
+  const [intro7Viz, setIntro7Viz] = React.useState(false)
+  const [intro8Viz, setIntro8Viz] = React.useState(false)
+
   return (
-    <Container
-      maxWidth="md"
-      style={{padding: "0 2.25rem"}}
-    >
-      <Grid
-        alignItems="center"
-        container
-        direction="column"
-        justify="center"
-        style={{minHeight: "100vh"}}
-        wrap="nowrap"
+    <>
+      <Head>
+        <title>Playtime Studios</title>
+      </Head>
+      <Container
+        maxWidth="md"
+        style={{padding: "0 3rem"}}
       >
-        <CssBaseline />
-        <Head>
-          <title>Create Next App</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Grid
-          container
-          component="main"
-          direction="column"
-          justify="center"
-          spacing={4}
-          style={{flex: 1}}
-          wrap="nowrap"
-        >
-          <Grid
-            item
-            xs={12}
-          >
-            <Typography
-              align="center"
-              component="h1"
-              gutterBottom
-              variant="h1"
-            >
-              Welcome to <Link href="https://nextjs.org">Next.js!</Link>
-            </Typography>
-            <Typography
-              align="center"
-              paragraph
-              variant="body1"
-            >
-              Get started by editing <Typography
-                style={{
-                  backgroundColor: "#eeeeee",
-                  borderRadius: "5px",
-                  fontFamily: [
-                    'Menlo',
-                    'Monaco',
-                    '"Lucida Console"',
-                    '"Liberation Mono"',
-                    '"DejaVu Sans Mono"',
-                    '"Bitstream Vera Sans Mono"',
-                    '"Courier New"',
-                    'monospace'
-                  ].join(', '),
-                  padding: "0.25rem"
-                }}
-                component="code"
-              >pages/index.js</Typography>
-            </Typography>
-          </Grid>
+        <Grid container direction="column" style={{minHeight: "100vh", paddingTop: "35vh", textAlign: "center"}}>
           <Grid item>
-            <Grid
-              container
-              spacing={4}
-              wrap="wrap"
-            >
-              <GridItem
-                description="Find in-depth information about Next.js features and API."
-                href="https://nextjs.org/docs"
-                title="Documentation"
-              />
-              <GridItem
-                description="Learn about Next.js in an interactive course with quizzes!"
-                href="https://nextjs.org/learn"
-                title="Learn"
-              />
-              <GridItem
-                description="Discover and deploy boilerplate example Next.js projects."
-                href="https://github.com/zeit/next.js/tree/master/examples"
-                title="Examples"
-              />
-              <GridItem
-                description="Instantly deploy your Next.js site to a public URL with Vercel."
-                href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                title="Deploy"
-              />
-            </Grid>
+            <Typography component="h1" style={{fontSize: "0"}}>
+              Playtime Studios
+            </Typography>
+            <img alt="Playtime Studios" height="auto" src="/logo.svg" width="100%" />
           </Grid>
         </Grid>
-        <Grid
-          alignItems="center"
-          container
-          component="footer"
-          style={{
-            borderTop: "1px solid #eaeaea",
-            height: "5em",
-            marginTop: "2.5rem"
-          }}
-          wrap="nowrap"
-        >
-          <Grid item style={{textAlign: "center"}} xs={12}>
-              <Link
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{color: "inherit", textDecoration: "none"}}
-              >
-                Powered by{' '}
-                <img
-                  alt="Vercel Logo"
-                  src="/vercel.svg"
-                  style={{height: "1rem", margin: "-0.2rem 0"}}
-                />
-              </Link>
+        <Grid container direction="column" style={{margin: "25vh 0"}}>
+          <VizSensor onChange={(isVisible) => { setIntro1Viz(isVisible) }} partialVisibility={true}>
+            <Fade in={intro1Viz} timeout={3000}>
+              <Grid item>
+                <Typography paragraph style={{color: "#888"}} variant="h2">
+                  Need excellent people who deliver excellent software solutions?
+                </Typography>
+                <Typography paragraph style={{color: "#888"}} variant="h2">
+                  Want to squeeze every last drop of potential from your team?
+                </Typography>
+                <Typography paragraph style={{color: "#888"}} variant="h2">
+                  Stuck in the mud but dream of flying above the clouds?
+                </Typography>
+              </Grid>
+            </Fade>
+          </VizSensor>
+        </Grid>
+        <Grid container direction="column" style={{margin: "50vh 0"}}>
+          <VizSensor onChange={(isVisible) => { setIntro2Viz(isVisible) }} partialVisibility={true}>
+            <Fade in={intro2Viz} timeout={3000}>
+              <Grid item>
+                <Typography gutterBottom variant="h2">
+                  Welcome
+                </Typography>
+                <Typography paragraph variant="h3" style={{color: "#888"}}>
+                  With over fifteen years experience Playtime Studios is rapidly growing as a leader in the software industry.
+                </Typography>
+                <Typography paragraph variant="h3" style={{color: "#888"}}>
+                  We work in several key disciplines with many satisfied clients delivering cost effective and value driven solutions.
+                </Typography>
+                <Typography paragraph variant="h3" style={{color: "#888"}}>
+                  Our deep understanding of the underlying fundamentals gives us the advantage over our competition.
+                </Typography>
+                <Typography paragraph variant="h3" style={{color: "#888"}}>
+                  Our commitment, whether working autonomously or augmenting your existing team, is to pass that advantage on to you.
+                </Typography>
+              </Grid>
+            </Fade>
+          </VizSensor>
+        </Grid>
+        <Grid container direction="column" style={{margin: "50vh 0"}}>
+          <VizSensor onChange={(isVisible) => { setIntro4Viz(isVisible) }} partialVisibility={true}>
+            <Fade in={intro4Viz} timeout={3000}>
+              <Grid item>
+                <Typography gutterBottom variant="h2">UX</Typography>
+                {/* Maybe these sections should be written in terms of cycles, eg, UX cycle, engineering cycle,... maybe doesn't work with the rest */}
+                <Typography paragraph variant="h3" style={{color: "#888"}}>
+                  The right user experience is essential.
+                </Typography>
+                <Typography paragraph variant="h3" style={{color: "#888"}}>
+                  User feedback, research, metrics, rapid application development, control and experimental.
+                </Typography>
+              </Grid>
+            </Fade>
+          </VizSensor>
+        </Grid>
+        <Grid container direction="column" style={{margin: "50vh 0"}}>
+          <VizSensor onChange={(isVisible) => { setIntro3Viz(isVisible) }} partialVisibility={true}>
+            <Fade in={intro3Viz} timeout={3000}>
+              <Grid item>
+                <Typography gutterBottom variant="h2">Engineering</Typography>
+                <Typography paragraph variant="h3" style={{color: "#888"}}>
+                  Relating to and creating a good user experience is relatively easy. Solid engineering to back it up is hard.
+                </Typography>
+              </Grid>
+            </Fade>
+          </VizSensor>
+        </Grid>
+        <Grid container direction="column" style={{margin: "50vh 0"}}>
+          <VizSensor onChange={(isVisible) => { setIntro5Viz(isVisible) }} partialVisibility={true}>
+            <Fade in={intro5Viz} timeout={3000}>
+              <Grid item>
+                <Typography gutterBottom variant="h2">Management</Typography>
+                <Typography paragraph variant="h3" style={{color: "#888"}}>
+                  Demystify the gap between business and software.
+                </Typography>
+                <Typography paragraph variant="h3" style={{color: "#888"}}>
+                  Understanding every aspect, agile through and through, taking the pain out of managment processes.
+                </Typography>
+              </Grid>
+            </Fade>
+          </VizSensor>
+        </Grid>
+        <Grid container direction="column" style={{margin: "50vh 0"}}>
+          <VizSensor onChange={(isVisible) => { setIntro6Viz(isVisible) }} partialVisibility={true}>
+            <Fade in={intro6Viz} timeout={3000}>
+              <Grid item>
+                <Typography gutterBottom variant="h2">Delivery</Typography>
+                <Typography paragraph variant="h3" style={{color: "#888"}}>
+                  Not the end but a new beginning.
+                </Typography>
+                <Typography paragraph variant="h3" style={{color: "#888"}}>
+                  At Playtime Studios we don&rsquo;t deliver a user experience and then we&rsquo;re done. That experience is another opportunity to improve and create additional experiences for our using.
+                </Typography>
+                <Typography paragraph variant="h3" style={{color: "#888"}}>
+                  User testing continues and metrics are collected to fuel new development.
+                </Typography>
+                <Typography paragraph variant="h3" style={{color: "#888"}}>
+                  Users continue exploratory testing, issues are identified, and resolved rapidly.
+                </Typography>
+              </Grid>
+            </Fade>
+          </VizSensor>
+        </Grid>
+        <Grid container direction="column" style={{margin: "50vh 0"}}>
+          <VizSensor onChange={(isVisible) => { setIntro7Viz(isVisible) }} partialVisibility={true}>
+            <Fade in={intro7Viz} timeout={3000}>
+              <Grid item>
+                <Typography gutterBottom variant="h2">Training</Typography>
+                <Typography paragraph variant="h3" style={{color: "#888"}}>
+                  Some text
+                </Typography>
+              </Grid>
+            </Fade>
+          </VizSensor>
+        </Grid>
+        <Grid container direction="column" style={{margin: "50vh 0"}}>
+          <VizSensor onChange={(isVisible) => { setIntro8Viz(isVisible) }} partialVisibility={true}>
+            <Fade in={intro8Viz} timeout={3000}>
+              <Grid item>
+                <Typography gutterBottom variant="h2">Simply Comprehensive</Typography>
+                <Typography paragraph variant="h3" style={{color: "#888"}}>
+                  Priced per service, iterative engagement with no further commitment.
+                </Typography>
+              </Grid>
+            </Fade>
+          </VizSensor>
+        </Grid>
+        <Grid container direction="column" style={{borderTop: "1px solid #888", marginTop: "50vh", padding: "2rem 2rem 3rem 2rem", textAlign: "center"}}>
+          <Grid item>
+            <Typography variant="body2">Playtime Studios (#12341243)...</Typography>
           </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </>
   )
 }
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,51 +1,62 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#0070f3"
+// Create a theme instance.
+let theme = createMuiTheme({
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        body: {
+          background: 'linear-gradient(190deg, #006699 0, #111 75rem)'
+        },
+        html: { backgroundColor: "#111"}
+      }
     }
   },
+  palette: { type: 'dark' },
   typography: {
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      'Oxygen',
-      'Ubuntu',
-      'Cantarell',
-      '"Fira Sans"',
-      '"Droid Sans"',
-      '"Helvetica Neue"',
-      'sans-serif'
-    ].join(', '),
-    fontSize: 16,
+    body1: {
+      color: "#EEE"
+    },
+    body2: {
+      color: "#EEE"
+    },
+    fontFamily: "Helvetica Neue",
     h1: {
+      color: "#EEE",
       fontSize: '4.209rem',
-      fontWeight: 700,
+      fontWeight: 500,
     },
     h2: {
+      color: "#DDD",
       fontSize: '3.157rem',
-      fontWeight: 700,
+      fontWeight: 500,
     },
     h3: {
+      color: "#CCC",
       fontSize: '2.369rem',
-      fontWeight: 700,
+      fontWeight: 500,
     },
     h4: {
+      color: "#BBB",
       fontSize: '1.777rem',
-      fontWeight: 700,
+      fontWeight: 500,
     },
     h5: {
+      color: "#AAA",
       fontSize: '1.333rem',
-      fontWeight: 700,
+      fontWeight: 500,
     },
     h6: {
+      color: "#999",
       fontSize: '1rem',
-      fontWeight: 700,
+      fontWeight: 500,
+    },
+    subtitle1: {
+      color: "#888"
     }
   }
 })
+
+theme = responsiveFontSizes(theme);
 
 export default theme;
