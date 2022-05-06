5834f2b5fff939e54dda50d4c1842f0624bcc80d
Rick Jones <rick.jones@playtimestudios.com>
2022-05-06 14:01:39 +0100

    Test

--- pages/index.tsx
+++ pages/index.tsx
@@ -179,8 +179,16 @@ export default function Index(): ReactElement {
         <title>Playtime Studios</title>
       </Head>
       <Script
-        id="hs-script-loader"
-        src="https://js-na1.hs-scripts.com/20268434.js"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+      (function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
+      vgo('setAccount', '1002197578');
+      vgo('setTrackByDefault', true);
+
+      vgo('process');
+            `,
+          }}
       />
       <Container maxWidth="md">
         <Grid
