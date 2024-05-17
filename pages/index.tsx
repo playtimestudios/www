4e520fe4fb0e396468cd78765918f537af6a83c4
Rick Jones <rick.jones@playtimestudios.com>
2024-05-17 14:19:42 +0100

    Further improve contact us buttons display on smaller screens
    Also, improved services column layout display on smaller screens.

--- pages/index.tsx
+++ pages/index.tsx
@@ -98,7 +98,7 @@ export default function Index(): ReactElement {
       <Container maxWidth="lg">
         <Grid container py={4} spacing={4}>
           <Grid>
-            <Grid container justifyContent="space-between" sx={{ flexDirection: { xs: "column-reverse", lg: "row" }}}>
+            <Grid container justifyContent="space-between" sx={{ flexDirection: { xs: "column-reverse", md: "row" }}}>
               <Grid>
                 <Typography gutterBottom variant="h2">
                   Greater Value Faster
@@ -123,13 +123,13 @@ export default function Index(): ReactElement {
             <img alt="Patchwork" src="/marvin-meyer-SYTO3xs06fU-unsplash.jpg" width="100%"/>
           </Grid>
           <Grid>
-            <Grid container justifyContent="space-between" sx={{ flexDirection: { xs: "column-reverse", lg: "row" }}}>
+            <Grid container justifyContent="space-between" sx={{ flexDirection: { xs: "column-reverse", md: "row" }}}>
               <Grid>
                 <Typography gutterBottom variant="h2">
                   Realise Your Objectives
                 </Typography>
               </Grid>
-              <Grid>
+              <Grid sx={{ display: { xs: "none", md: "block" }}}>
                 <Typography gutterBottom variant="h2" component="div">
                   <Button variant="contained" disableRipple href="#contact">Contact Us</Button>
                 </Typography>
@@ -141,17 +141,23 @@ export default function Index(): ReactElement {
           </Grid>
         </Grid>
         <Grid container spacing={4}>
-          <Grid xs={12} lg={6}>
-            <Typography align="center" gutterBottom variant="h3">Software Engineering</Typography>
-            <Typography align="center" gutterBottom variant="subtitle1">Building rock-solid software that solves your unique challenges</Typography>
+          <Grid xs={12} md={6}>
+            <Typography gutterBottom variant="h3" component="div" sx={{ display: { xs: "block", md: "none" }}}>
+              <Button variant="contained" disableRipple href="#contact">Contact Us</Button>
+            </Typography>
+            <Typography sx={{ textAlign: { md: "center" }}} gutterBottom variant="h3">Software Engineering</Typography>
+            <Typography sx={{ textAlign: { md: "center" }}} gutterBottom variant="subtitle1">Building rock-solid software that solves your unique challenges</Typography>
             <p><img alt="Test" src="/scott-graham-5fNmWej4tAA-unsplash.jpg" style={{width: "100%"}} /></p>
             <Typography gutterBottom variant="body1">
               In today&apos;s digital world, innovative software is the backbone of any successful business. We&apos;re a team of passionate engineers who specialize in crafting custom software solutions tailored to your specific needs. We&apos;ll work closely with you to understand your goals, challenges, and target audience.  We then leverage our expertise in cutting-edge technologies to build secure, scalable, and user-friendly software that delivers real results.
             </Typography>
           </Grid>
-          <Grid xs={12} lg={6}>
-            <Typography align="center" gutterBottom variant="h3">Customer Development</Typography>
-            <Typography align="center" gutterBottom variant="subtitle1">Unlocking your full customer potential</Typography>
+          <Grid xs={12} md={6}>
+            <Typography gutterBottom variant="h3" component="div" sx={{ display: { xs: "block", md: "none" }}}>
+              <Button variant="contained" disableRipple href="#contact">Contact Us</Button>
+            </Typography>
+            <Typography sx={{ textAlign: { md: "center" }}} gutterBottom variant="h3">Customer Development</Typography>
+            <Typography sx={{ textAlign: { md: "center" }}} gutterBottom variant="subtitle1">Unlocking your full customer potential</Typography>
             <p><img alt="Test" src="/john-schnobrich-2FPjlAyMQTA-unsplash.jpg" style={{width: "100%"}} /></p>
             <Typography gutterBottom variant="body1">
               Do you know what your customers truly want and need? At Playtime Studios, our customer development experts help you bridge the gap between your business and your target audience. We utilize a data-driven approach to understand customer behavior, identify pain points, and develop strategies to acquire, retain, and grow your customer base.            </Typography>
